eefe9e936cee6eb9cdc8f4bbcafdd1ec9f4d48f0
HAVEHA <1040186242@qq.com>
2020-09-24 07:52:39 +0800

    update/9/24

--- src/main/java/com/wei/myblog/MyblogApplication.java
+++ src/main/java/com/wei/myblog/MyblogApplication.java
@@ -2,6 +2,10 @@ package com.wei.myblog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
@@ -9,5 +13,10 @@ public class MyblogApplication {
     public static void main(String[] args) {
         SpringApplication.run(MyblogApplication.class, args);
     }
-
+    @Bean
+    public ConfigurableServletWebServerFactory webServerFactory() {
+        TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
+        factory.addConnectorCustomizers((TomcatConnectorCustomizer) connector -> connector.setProperty("relaxedQueryChars", "|{}[]\\"));
+        return factory;
+    }
 }
